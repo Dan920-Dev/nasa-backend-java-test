638c0ea72d85dd2e489e0830ac4c59e7268221f7
Dan920-Dev <00138218@uca.edu.sv>
2024-02-07 21:34:49 -0600

    Adding entity and lombook dependency

--- src/main/java/com/nasa/prueba/aspirante/dominio/entities/PruebaEntity.java
+++ src/main/java/com/nasa/prueba/aspirante/dominio/entities/PruebaEntity.java
@@ -1,5 +1,33 @@
 package com.nasa.prueba.aspirante.dominio.entities;
 
-public class PruebaEntity {
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "nasa_content")
+@Data
+public class PruebaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "href")
+    private String href;
+
+    @Column(name = "center")
+    private String center;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "nasa_id")
+    private String nasaId;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private Date dateCreated;
 }
