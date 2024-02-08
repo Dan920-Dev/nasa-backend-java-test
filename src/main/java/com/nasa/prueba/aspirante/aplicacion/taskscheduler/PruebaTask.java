a1794fb611d9c072cab616cd7d3b46756d28e4f2
Dan920-Dev <00138218@uca.edu.sv>
2024-02-07 23:27:50 -0600

    Congig task and update Api connection class

--- src/main/java/com/nasa/prueba/aspirante/aplicacion/taskscheduler/PruebaTask.java
+++ src/main/java/com/nasa/prueba/aspirante/aplicacion/taskscheduler/PruebaTask.java
@@ -1,5 +1,21 @@
 package com.nasa.prueba.aspirante.aplicacion.taskscheduler;
 
+
+import com.nasa.prueba.aspirante.dominio.dto.ApiResponseDTO;
+import com.nasa.prueba.aspirante.infraestructura.clientrest.PruebaClienteRest;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
 public class PruebaTask {
 
+    private PruebaClienteRest test;
+
+    @Scheduled( fixedRate = 60000)
+    public void task(){
+        System.out.println("Se realizo accion..");
+        // ApiResponseDTO response = test.getDataToAPI();
+    }
 }
--- src/main/java/com/nasa/prueba/aspirante/infraestructura/clientrest/PruebaClienteRest.java
+++ src/main/java/com/nasa/prueba/aspirante/infraestructura/clientrest/PruebaClienteRest.java
@@ -1,15 +1,16 @@
 package com.nasa.prueba.aspirante.infraestructura.clientrest;
 
 import com.nasa.prueba.aspirante.dominio.dto.ApiResponseDTO;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class PruebaClienteRest {
     String url = "https://images-api.nasa.gov/search?q=apollo%2011";
     private final RestTemplate restTemplate = new RestTemplate();
 
-    private ApiResponseDTO getDataToAPI(){
+    public ApiResponseDTO getDataToAPI(){
         return restTemplate.getForObject(url, ApiResponseDTO.class);
     }
-
 
 }
