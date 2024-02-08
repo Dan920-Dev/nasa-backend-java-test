abe5a2376a18902346cc0036901a8b3850b2e718
Dan920-Dev <00138218@uca.edu.sv>
2024-02-07 22:15:31 -0600

    Adding template to controller and adding jackson databind to map the json response

--- src/main/java/com/nasa/prueba/aspirante/infraestructura/clientrest/PruebaClienteRest.java
+++ src/main/java/com/nasa/prueba/aspirante/infraestructura/clientrest/PruebaClienteRest.java
@@ -1,5 +1,6 @@
 package com.nasa.prueba.aspirante.infraestructura.clientrest;
 
 public class PruebaClienteRest {
+    String root = "https://images-api.nasa.gov";
 
 }
--- src/main/java/com/nasa/prueba/aspirante/infraestructura/restcontroller/PruebaRestController.java
+++ src/main/java/com/nasa/prueba/aspirante/infraestructura/restcontroller/PruebaRestController.java
@@ -1,5 +1,19 @@
 package com.nasa.prueba.aspirante.infraestructura.restcontroller;
 
-public class PruebaRestController {
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/nasa/test/")
+public class PruebaRestController {
+    // This is a template
+
+    @GetMapping("getAll")
+    ResponseEntity<?> getAllData(){
+        return new ResponseEntity<>("Test Done",HttpStatus.ACCEPTED);
+    }
 }
